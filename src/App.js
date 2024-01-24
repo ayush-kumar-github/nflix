544967922041498401e5b255d73856b7fc50769c
Ayush Kumar <aykr571@gmail.com>
2024-01-24 21:05:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import React from "react";
-import Body from "./components/Body";
-import { Provider } from "react-redux";
-import appStore from "./utils/appStore";
 
 const App = () => {
-  return (
-    <Provider store={appStore}>
-      <Body />
-    </Provider>
-  );
+  return <div>App</div>;
 };
 
 export default App;
